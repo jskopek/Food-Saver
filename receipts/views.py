a06193d09f72617b97ea770c5814b5a65f49bb41
Jean-Marc Skopek <jskopek@gmail.com>
2012-09-29 14:38:50 -0700

    Set up proof of concept SMS tracking

--- receipts/views.py
+++ receipts/views.py
@@ -2,12 +2,15 @@ from receipts.models import Receipt
 from members.models import Member
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
+@csrf_exempt
 def scan(request):
-    phone_number = request.GET["phone_number"]
-    code = request.GET["code"]
+    import ipdb; ipdb.set_trace()
+    phone_number = request.POST["From"]
+    code = request.POST["Body"]
 
     receipt = get_object_or_404(Receipt, code=code)
     member, created = Member.objects.get_or_create(phone_number=phone_number)
@@ -15,6 +18,11 @@ def scan(request):
     receipt.member = member
     receipt.save()
 
+
+    return render(request, "receipts/scan_xml.html", {
+        "receipt": receipt,
+        "member": member
+    })
     return render(request, "receipts/scan.html", {
         "receipt": receipt,
         "member": member
