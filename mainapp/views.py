7a6c4e15a18299822d8ed74fb01cfb4519a919fd
Jean-Marc Skopek <jskopek@gmail.com>
2012-09-30 00:54:49 -0700

    Styling tweaks

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.conf import settings
 from datetime import date
+from receipts.models import Receipt
 
 
 # Create your views here.
 def home(request):
+    receipts = Receipt.objects.all()
+
     return render(request, "mainapp/home.html", {
-        "phone": settings.TWILIO_PHONE,
+        "receipts": receipts,
+        "phone": settings.TWILIO_PHONE_CLEAN,
         "months": range(1, 13),
         "days": range(1, 32)
     })
